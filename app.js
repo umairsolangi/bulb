b1941661ec6f7564ff96299308a2e0e4190623ff
umairsolangi <51870556+umairsolangi@users.noreply.github.com>
2020-07-06 21:47:49 +0500

    Bulb On Off Using JavaScript
    Bulb On Off

---
+++ app.js
@@ -0,0 +1,8 @@
+function onbulb(){
+    var onbulb = document.getElementById("bulb")
+    onbulb.src = "images/onbulb.jpg"
+}
+function offbulb(){
+    var offbulb = document.getElementById("bulb")
+    offbulb.src = "images/offbulb.jpg"
+}
